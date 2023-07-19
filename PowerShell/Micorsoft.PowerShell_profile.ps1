f92163bf3194f5a16caae0a01d706f0f9929cf69
bodav <bodavidsen@gmail.com>
2023-07-19 08:26:47 +0200

    Update Micorsoft.PowerShell_profile.ps1

--- PowerShell/Micorsoft.PowerShell_profile.ps1
+++ PowerShell/Micorsoft.PowerShell_profile.ps1
@@ -1,8 +1,3 @@
-New-Alias g goto
-
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/powerlevel10k_rainbow.omp.json" | Invoke-Expression
-#& ([ScriptBlock]::Create((oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\powerlevel10k_rainbow.omp.json" --print) -join "`n"))
-
 $env:Path += ";C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64"
 $env:Path += ";C:\dev\cmdtools"
 
@@ -26,6 +21,60 @@ Set-PSReadLineOption @PSROptions
 
 #Install-Module -Name Terminal-Icons -Repository PSGallery
 Import-Module -Name Terminal-Icons
+
+function prompt {
+
+    $CmdWasSuccessfull = $?
+
+    $host.ui.RawUI.WindowTitle = "$pwd"
+    $CmdPromptCurrentFolder = Split-Path -Path $pwd -Leaf
+    $IsHome = $pwd.Path -eq $HOME
+
+    $inGitRepo = (git rev-parse --is-inside-work-tree 2> $null)
+
+    $IsAdmin = (New-Object Security.Principal.WindowsPrincipal ([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+
+    $LastCommand = Get-History -Count 1
+    if ($lastCommand) { 
+        $RunTime = ($lastCommand.EndExecutionTime - $lastCommand.StartExecutionTime)
+    }
+
+    if ($RunTime.TotalSeconds -ge 60) {
+        $ElapsedTime = $RunTime.ToString("mm\:ss")
+    }
+    elseif ($RunTime.TotalSeconds -lt 1) {
+        $ElapsedTime = -join ([math]::Round(($RunTime.TotalMilliseconds), 0), "ms")
+    }
+    else {
+        $ElapsedTime = -join ([math]::Round(($RunTime.TotalSeconds), 2), "s")
+    }
+
+    Write-Host ""
+    Write-host ($(if ($IsAdmin) { ' Elevated ' } else { '' })) -BackgroundColor DarkRed -ForegroundColor White -NoNewline
+    Write-Host " $($env:USERNAME)@$($env:COMPUTERNAME) " -BackgroundColor DarkCyan -ForegroundColor Black -NoNewline
+
+    If ($IsHome) { Write-Host "  Home "  -ForegroundColor White -BackgroundColor DarkGray -NoNewline }
+    ElseIf ($CmdPromptCurrentFolder -like "*:*") { Write-Host " $CmdPromptCurrentFolder "  -ForegroundColor White -BackgroundColor DarkGray -NoNewline }
+    Else { Write-Host " .\$CmdPromptCurrentFolder\ "  -ForegroundColor White -BackgroundColor DarkGray -NoNewline }
+
+    Write-Host "" -NoNewline
+
+    if ($inGitRepo) {
+        $gitBranch = git rev-parse --abbrev-ref HEAD
+        Write-Host "  $($gitBranch) " -BackgroundColor Yellow -ForegroundColor Black -NoNewline
+    }
+
+    Write-Host " $elapsedTime " -BackgroundColor Green -ForegroundColor Black -NoNewline
+    Write-Host ""
+    
+    If ($CmdWasSuccessfull) {
+        Write-Host "=>" -ForegroundColor Green -NoNewline
+    }
+    else {
+        Write-Host "X" -ForegroundColor Red -NoNewline
+    }
+    return " "
+}
 
 function goto {
     param (
@@ -51,6 +100,8 @@ function goto {
     }
 }
 
+Set-Alias -Name g -Value goto
+
 function touch {
     param(
         $filename
@@ -68,6 +119,16 @@ function Get-AdUser {
 }
 
 Set-Alias -Name aduser -Value Get-AdUser
+
+function Get-AdGroup {
+    param(
+        $groupname
+    )
+
+    (New-Object System.DirectoryServices.DirectoryEntry((New-Object System.DirectoryServices.DirectorySearcher("(&(objectCategory=Group)(name=$($groupname)))")).FindOne().GetDirectoryEntry().Path)).member | % { (New-Object System.DirectoryServices.DirectoryEntry("LDAP://" + $_)) } | Sort-Object sAMAccountName | SELECT @{name = "User Name"; expression = { $_.Name } }, @{name = "User sAMAccountName"; expression = { $_.sAMAccountName } }
+}
+
+Set-Alias -Name adgroup -Value Get-AdGroup
 
 function ll { 
     Get-ChildItem -Path $pwd -File 
